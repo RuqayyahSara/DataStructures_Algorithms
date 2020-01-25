59d4d091f875e6abb621437e409c98f98a5a80a3
RuqayyahSara <sarasadiya4@gmail.com>
2020-01-25 23:27:51 +0530

    node insertion at head

--- DS/SinglyLList.js
+++ DS/SinglyLList.js
@@ -10,3 +10,19 @@ class LinkedList{
         this.head=null;
         this.tail=null;
     }
+
+    addToHead(value){
+        const newNode = new Node(value,this.head);
+        if(!this.head){
+            this.tail = newNode;
+        }
+        this.head= newNode;  
+     }
+
+}   
+
+const ll = new LinkedList();
+ll.addToHead(100);
+ll.addToHead(200);
+
+console.log(ll);
