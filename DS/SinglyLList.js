63a38ac38cfb2fa1c8c577d3c2406bb7e5404b12
RuqayyahSara <sarasadiya4@gmail.com>
2020-01-25 23:38:31 +0530

    node insertion at tail

--- DS/SinglyLList.js
+++ DS/SinglyLList.js
@@ -18,11 +18,21 @@ class LinkedList{
         }
         this.head= newNode;  
      }
-
+    
+     addToTail(value){
+        const newNode = new Node(value,null);
+        if(!this.tail){
+            this.head = newNode;
+        }
+        else{
+        this.tail.next = newNode;
+    }
+    this.tail= newNode;  
+     }
 }   
 
 const ll = new LinkedList();
 ll.addToHead(100);
 ll.addToHead(200);
-
+ll.addToTail(700);
 console.log(ll);
